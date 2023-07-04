f5e27f148f015c2940229875460e8e3e391f931c
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2023-07-05 01:29:41 +0300

    update

---
+++ CLion/CP CLion Template/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout <<"YES\n"
+#define no cout << "NO\n"
+#define all(v) v.begin(),v.end()
+#define out(v) for(auto it : v) { cout << it << " ";}
+#define PI acos(-1)
+ll mod = 1e9 + 7;
+void Go() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+//*************
+void doIt() {
+    // solve here
+
+}
+//*************
+int main() {
+    Go();
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++) {
+        doIt();
+    }
+}
