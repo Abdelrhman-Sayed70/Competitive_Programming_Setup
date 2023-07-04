53a1eb1234a476be687ba4248111a8a860d687b8
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2023-07-05 01:13:21 +0300

    update

---
+++ VS Code/CP VSC Template/Solution/main.cpp
@@ -0,0 +1,32 @@
+#include<bits\stdc++.h>
+#include<unordered_map>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define yes cout <<"YES\n"
+#define no cout << "NO\n"
+#define all(v) v.begin(),v.end()
+#define out(v) for(auto it : v) { cout << it << " ";}
+#define PI acos(-1)
+ll mod = 1e9 + 7;
+void Go() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+//*************
+void doIt() {
+    // Solve here
+    cout << "Hello World";
+}
+//*************
+int main() {
+    Go();
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        doIt();
+    }
+}
